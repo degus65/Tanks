099934b3484a816deb83e3dafa5697fe02794efc
degus65 <nowik2@gmail.com>
2015-10-30 17:23:53 +0100

    Okno
    Dodane:
    -ramka i panel ze sceną gry;
    -gracz i możliwość poruszania się po mapie;

---
+++ src/Player.java
@@ -0,0 +1,93 @@
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+
+
+public class Player {
+	private int x=400, y=400, vX=0, vY=0;
+	private static final int PLAYER_SPEED = 4;
+	String sciezka;
+	SpriteCache sprite=new SpriteCache();
+	private boolean up=false, down=false, left=false, right=false;
+	
+	Player(String sciezka)
+	{
+		this.sciezka=sciezka;
+	}
+	
+	public void act() {
+		x+=vX;
+		y+=vY;
+	}
+	
+	protected void updateSpeed()
+	{
+		vX=0;
+		vY=0;
+		if (down)
+		{
+			vY = PLAYER_SPEED;
+			this.sciezka="playerDown.png";
+		}
+		if (up) 
+		{
+			vY = -PLAYER_SPEED;
+			this.sciezka="playerUp.png";
+		}
+		if (left) 
+		{
+			vX = -PLAYER_SPEED;
+			this.sciezka="playerLeft.png";
+		}
+		if (right) 
+		{
+			vX = PLAYER_SPEED;
+			this.sciezka="playerRight.png";
+		}
+	}
+
+	public void keyPressed(KeyEvent k) 
+	{
+		if(k.getKeyCode()==KeyEvent.VK_RIGHT)
+			right=true;
+		if(k.getKeyCode()==KeyEvent.VK_LEFT)
+			left=true;
+		if(k.getKeyCode()==KeyEvent.VK_UP)
+			up=true;
+		if(k.getKeyCode()==KeyEvent.VK_DOWN)
+			down=true;
+		updateSpeed();
+	}
+	
+	public void keyReleased(KeyEvent k)
+	{
+		if(k.getKeyCode()==KeyEvent.VK_RIGHT)
+			right=false;
+		if(k.getKeyCode()==KeyEvent.VK_LEFT)
+			left=false;
+		if(k.getKeyCode()==KeyEvent.VK_UP)
+			up=false;
+		if(k.getKeyCode()==KeyEvent.VK_DOWN)
+			down=false;
+		updateSpeed();
+	}
+	
+	public BufferedImage getSprite()
+	{
+		return sprite.getSprite(sciezka);
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
+	
+	public void setSciezka(String sciezka)
+	{
+		this.sciezka=sciezka;
+	}
+		
+}
---
+++ src/Stage.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+
+
+import javax.swing.Timer;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+public class Stage extends JPanel implements KeyListener, ActionListener {
+	
+	public static final int SZEROKOSC = 700;
+	public static final int WYSOKOSC = 500;
+	public long usedTime=0;
+	
+	Player p1=new Player("playerUp.png");
+	
+	
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(p1.getSprite(), p1.getX(), p1.getY(),this);
+		g.setColor(Color.WHITE);
+		if (usedTime > 0)
+			g.drawString(String.valueOf(1000/usedTime)+" fps",5,WYSOKOSC-50);
+		else 
+			g.drawString("--- fps",5,WYSOKOSC-50);
+		
+	}
+	
+	public void updateWorld()
+	{
+		p1.act();
+	}
+	
+	public void gameLoop()
+	{
+		long startTime = System.currentTimeMillis();
+		updateWorld();
+		repaint();
+		usedTime = System.currentTimeMillis()-startTime;
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent k) {
+		// TODO Auto-generated method stub
+		p1.keyPressed(k);
+	}
+	@Override
+	public void keyReleased(KeyEvent k) {
+		// TODO Auto-generated method stub
+		p1.keyReleased(k);
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		gameLoop();
+	}
+	
+	Stage()
+	{
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(new BorderLayout(0, 0));
+		
+		Timer timer = new Timer(1000/60, (ActionListener) this);
+        timer.start();
+        
+		setFocusable(true);
+		addKeyListener(this);
+	}
+
+	
+}
